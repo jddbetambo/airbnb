ef4cd091e46239844531ad539178bdb48aa65304
jddbetambo <jddbetambo@gmail.com>
2024-10-31 21:49:27 -0500

    Adding project Source Files.

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.Access_Key_ID
+  secret_key = var.Access_Key_Secret
+}
---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-06b21ccaeff8cd686"
+  description = "instance ami used"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "Instance type used"
+}
+
+variable "Access_Key_ID" {
+  type        = string
+  description = "Your User Access Key ID for security and running ec2 commands"
+  default     = "AKIAW3MEBM6WZ2NNFJWV"
+}
+
+variable "Access_Key_Secret" {
+  type        = string
+  description = "Your User Secret Access Key for security and running ec2 commands"
+  default     = "hfQtpDdQfuwqMIdMmPJfJNnQidFu3Fa/fDvjPIBl"
+}
