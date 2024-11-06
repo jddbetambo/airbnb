f51296b503be31c0a65fd19ce2a1a518a6b4f0ab
jddbetambo <jddbetambo@gmail.com>
2024-11-06 13:44:55 -0600

    Update main.tf with security

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  ebs_optimized = true
+
+  root_block_device {
+    encrypted     = true
+  }
 
   tags = {
     Name = "airbnb-web-server"
